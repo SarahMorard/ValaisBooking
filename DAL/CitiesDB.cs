6b3ad349e85c93ea7f2186de5abd8f366f34dc32
Sarah <sarah.morard@students.hevs.ch>
2019-11-06 09:00:22 +0100

    Correction IdCity

--- DAL/CitiesDB.cs
+++ DAL/CitiesDB.cs
@@ -66,7 +66,7 @@ namespace DAL
             {
                 using (SqlConnection cn = new SqlConnection(connectionString))
                 {
-                    string query = "Select * from Cities where idCities = @idCities";
+                    string query = "Select * from Cities where IdCities = @idCities";
                     SqlCommand cmd = new SqlCommand(query, cn);
                     cmd.Parameters.AddWithValue("@idCities", idCities);
 
@@ -78,7 +78,7 @@ namespace DAL
                         if (dr.Read())
                         {
                              cities= new Cities();
-                            cities.idCities = (int)dr["idCities"];
+                            cities.idCities = (int)dr["IdCities"];
                             cities.zip_code = (int)dr["zip_code"];
                             cities.name = (string)dr["name"];
                         }
